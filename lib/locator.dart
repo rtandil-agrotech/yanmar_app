eeb93207ee9a435f45d5809d4bdfba95a086e05c
Riko Tan <riktandil@gmail.com>
2024-08-10 11:02:04 +0700

    feat: remove postgresql

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,25 +1,6 @@
 import 'package:get_it/get_it.dart';
-import 'package:postgres/postgres.dart';
 import 'package:yanmar_app/env/env.dart';
-import 'package:yanmar_app/repository/db_repository.dart';
 
 final locator = GetIt.instance;
 
-Future<void> setupAsync() async {
-  final conn = await _setupDBConn();
-
-  locator.registerLazySingleton<DBRepository>(() => DBRepository(conn));
-}
-
-Future<Connection> _setupDBConn() async {
-  final conn = await Connection.open(Endpoint(
-    host: Env.dbHost,
-    database: Env.dbName,
-    username: Env.dbName,
-    password: Env.dbPassword,
-  ));
-
-  if (!conn.isOpen) throw Exception('Connection not open');
-
-  return conn;
-}
+Future<void> setupAsync() async {}
